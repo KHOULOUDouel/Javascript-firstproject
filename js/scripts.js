1e6e193213dfe8ec132abcc0fda58eeaa5626ad7
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-03-05 00:47:55 +0100

    Create a new variable called pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-
+let pokemonList = [
+    { name: 'Charizard' height: '1' type: 'poison'  }
+    { name: 'Pikachu' height: '0.4' type: 'poison'  }
+    { name: 'Clefairy' height: '0.6' type: 'grass'  }
+    { name: 'Growlithe' height: '0.7' type: 'grass' }
+]
