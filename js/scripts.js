321d511b81b964ecb216f0039e0716408e6175e4
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-03-05 14:33:30 +0100

    Fix Data Types andSyntax Errors and Array declaration

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-    { name: 'Charizard' height: '1' type: 'poison'  }
-    { name: 'Pikachu' height: '0.4' type: 'poison'  }
-    { name: 'Clefairy' height: '0.6' type: 'grass'  }
-    { name: 'Growlithe' height: '0.7' type: 'grass' }
-]
+    { name: 'Charizard',height:1 ,Types: 'poison'  },
+    { name: 'Pikachu', height: 0.4 ,Types: 'grass'  },
+    { name: 'Clefairy', height: 0.6 ,Types: 'normal'  },
+    { name: 'Growlithe', height: 0.7 ,Types: 'ghost' },
+];
