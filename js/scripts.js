8d06c9925330dca77a10afb1724639c15f4be5e2
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-03-07 02:30:33 +0100

    create a for loop
    create a for loop that iterates over each item in pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
+
 let pokemonList = [
-    { name: 'Charizard',height:1 ,Types: 'poison'  },
-    { name: 'Pikachu', height: 0.4 ,Types: 'grass'  },
-    { name: 'Clefairy', height: 0.6 ,Types: 'normal'  },
-    { name: 'Growlithe', height: 0.7 ,Types: 'ghost' },
+    { name: 'Charizard', height: 1, Types: ['poison'] },
+    { name: 'Pikachu', height: 0.4, Types: ['grass'] },
+    { name: 'Clefairy', height: 0.6, Types: ['normal'] },
+    { name: 'Growlithe', height: 0.7, Types: ['ghost'] },
 ];
+
+let pokemon = pokemonList[i];
+ for (let i = 0; i < pokemonList.length; i++) {
+    onsole.log(`Name: ${pokemon.name}, Height: ${pokemon.height}m, Types: ${pokemon.types}`);
+ }
+
+
