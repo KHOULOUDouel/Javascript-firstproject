5aa5a27d8c1297e0b1ec73f24d8134487885f7a6
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-03-12 16:30:54 +0100

    Move JavaScript code to the scripts.js
    Move the JavaScript code within the <script> tag from the index.html file to the scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
-
 let pokemonList = [
-    { name: 'Charizard', height: 1, Types: ['poison'] },
-    { name: 'Pikachu', height: 0.4, Types: ['grass'] },
-    { name: 'Clefairy', height: 0.6, Types: ['normal'] },
-    { name: 'Growlithe', height: 0.7, Types: ['ghost'] },
+  { name: "Charizard", height: 1, types: ["poison"] },
+  { name: "Pikachu", height: 0.4, types: ["grass"] },
+  { name: "Clefairy", height: 0.6, types: ["normal"] },
+  { name: "Growlithe", height: 0.7, types: ["ghost"] },
 ];
 
-// create a for loop that iterates over each item in pokemonList
-let pokemon = pokemonList[i];
- for (let i = 0; i < pokemonList.length; i++) {
-    onsole.log(`Name: ${pokemon.name}, Height: ${pokemon.height}m, types: ${pokemon.types}`);
- }
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemon = pokemonList[i];
 
+  // Check if the height is above a certain value
+  let isTall = pokemon.height > 0.6;
 
+  // Use document.write() to display the Pokémon with name and height
+  document.write(`<p>${pokemon.name} (height: ${pokemon.height})`);
+
+  // Add a note if the height is above a certain value
+  if (isTall) {
+    document.write(" - Wow, that’s big!");
+  }
+}
