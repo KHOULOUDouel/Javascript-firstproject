f2c8e47b0603e2a1502af935d4e62a30e8e45e1a
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-03-16 14:33:19 +0100

    Define pokemonRepository IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,8 @@
-(function() {
-
-let pokemonList = [
-  { name: "Charizard", height: 1, types: ["poison"] },
-  { name: "Pikachu", height: 0.4, types: ["grass"] },
-  { name: "Clefairy", height: 0.6, types: ["normal"] },
-  { name: "Growlithe", height: 0.7, types: ["ghost"] },
-];
-
-pokemonList.forEach(function(pokemon) {
-  let isTall = pokemon.height > 0.6;
-
-  // Use document.write() to display the Pokémon with name and height
-  document.write(`<p>${pokemon.name} (height: ${pokemon.height})`);
-
-  // Add a note if the height is above a certain value
-  if (isTall) {
-    document.write(" : Wow, that’s big!");
-  }
-}
-
-})();
+// Define pokemonRepository IIFE
+let pokemonRepository = (function() {
+  let pokemonList = [
+    { name: "Charizard", height: 1, types: ["poison"] },
+    { name: "Pikachu", height: 0.4, types: ["grass"] },
+    { name: "Clefairy", height: 0.6, types: ["normal"] },
+    { name: "Growlithe", height: 0.7, types: ["ghost"] },
+  ];
