8545e8e594ad7e4b98b9e4de4421f37b14574d72
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-03-25 22:05:51 +0100

    removing the array of Pokémon objects and replace it with an empty array

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
 // Define pokemonRepository IIFE
 let pokemonRepository = (function() {
-  let pokemonList = [
-    { name: "Charizard", height: 1, types: ["poison"] },
-    { name: "Pikachu", height: 0.4, types: ["grass"] },
-    { name: "Clefairy", height: 0.6, types: ["normal"] },
-    { name: "Growlithe", height: 0.7, types: ["ghost"] },
-  ];
+  let pokemonList = [];
 
   // Define add and getAll functions
   function add(pokemon) {
